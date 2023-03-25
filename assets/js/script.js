498cd4cca537c93a3359f9d830d85e42a5e92474
andrewcannan <andrewmcannan@gmail.com>
2023-03-25 15:16:23 +0000

    added multiply game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,9 @@ function runGame(gameType){
     if (gameType === 'addition'){
         displayAdditionQuestion(num1, num2);
     }
+    else if (gameType === 'multiply'){
+        displayMultiplyQuestion(num1, num2);
+    }
     else {
         alert(`unknown game type: ${gameType}`);
         throw(`unknown game type: ${gameType}. Aborting!`);
@@ -66,8 +69,11 @@ function calculateCorrectAnswer(){
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById('operator').innerText;
 
-    if (operator = '+'){
+    if (operator === '+'){
         return [operand1 + operand2, "addition"];
+    }
+    else if (operator === 'x'){
+        return [operand1 * operand2, "multiply"];
     }
     else {
         alert(`unknown operator ${operator}`);
@@ -102,8 +108,10 @@ function displaySubtractQuestion(){
     
 };
 
-function displayMultiplyQuestion(){
-    
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 };
 
 function displayDivisionQuestion(){
